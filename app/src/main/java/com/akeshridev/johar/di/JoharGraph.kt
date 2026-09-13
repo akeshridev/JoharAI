8d34195f636b89ca0ba0d4e367fb48e0c80e97b1
akeshridev <akeshridev@gmail.com>
2026-09-13 22:59:50 +0530

    Simplify Johar graph for prebuilt database

--- app/src/main/java/com/akeshridev/johar/di/JoharGraph.kt
+++ app/src/main/java/com/akeshridev/johar/di/JoharGraph.kt
@@ -1,17 +1,11 @@
 package com.akeshridev.johar.di
 
 import android.content.Context
-import com.akeshridev.johar.data.pack.KnowledgePackLoader
 import com.akeshridev.johar.data.work.WorkManagerSourceCrawlScheduler
 import com.akeshridev.johar.domain.crawl.ScheduleSourceCrawlUseCase
 import com.akeshridev.johar.domain.crawl.SourceCrawlScheduler
 
 class JoharGraph(context: Context) {
-    private val appContext = context.applicationContext
-    private val scheduler: SourceCrawlScheduler = WorkManagerSourceCrawlScheduler(appContext)
-    private val knowledgePackLoader = KnowledgePackLoader(appContext)
-
+    private val scheduler: SourceCrawlScheduler = WorkManagerSourceCrawlScheduler(context.applicationContext)
     val scheduleSourceCrawlUseCase = ScheduleSourceCrawlUseCase(scheduler)
-
-    fun loadKnowledgePack() = knowledgePackLoader.load()
 }
