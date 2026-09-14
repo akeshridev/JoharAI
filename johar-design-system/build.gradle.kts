d424591be6f0f1b99e79d1fcce7d6600fc98cd75
akeshridev <akeshridev@gmail.com>
2026-09-14 16:54:53 +0530

    Add foundation dependency for design system components

--- johar-design-system/build.gradle.kts
+++ johar-design-system/build.gradle.kts
@@ -26,6 +26,7 @@ dependencies {
     implementation(composeBom)
 
     implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.foundation:foundation")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
 
