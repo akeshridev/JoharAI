3541bda8c1606eebdea0e3dd034ef5cd238ba106
akeshridev <akeshridev@gmail.com>
2026-09-14 20:28:08 +0530

    Wire agent command assets into Android instrumentation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,6 +24,8 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
+
+    sourceSets["androidTest"].assets.srcDir(rootProject.file("tests/agent/assets"))
 }
 
 dependencies {
