cf4c3a682f702487e2e321f370ef014d944adc8c
akeshridev <akeshridev@gmail.com>
2026-09-14 16:53:08 +0530

    Fix card token imports

--- johar-design-system/src/main/java/com/akeshridev/johar/designsystem/JoharCards.kt
+++ johar-design-system/src/main/java/com/akeshridev/johar/designsystem/JoharCards.kt
@@ -16,6 +16,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun JoharPlaceCard(
