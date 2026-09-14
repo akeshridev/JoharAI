a969a03ae85a3d6fdc2782b27bee4e3eb4444687
akeshridev <akeshridev@gmail.com>
2026-09-14 13:33:51 +0530

    Make Ranchi the default prototype crawl target

--- johar-domain/src/main/kotlin/com/akeshridev/johar/domain/crawl/ScheduleSourceCrawlUseCase.kt
+++ johar-domain/src/main/kotlin/com/akeshridev/johar/domain/crawl/ScheduleSourceCrawlUseCase.kt
@@ -3,7 +3,7 @@ package com.akeshridev.johar.domain.crawl
 class ScheduleSourceCrawlUseCase(
     private val scheduler: SourceCrawlScheduler,
 ) {
-    operator fun invoke(target: CrawlTarget = CrawlTarget.JHARKHAND) {
+    operator fun invoke(target: CrawlTarget = CrawlTarget.RANCHI) {
         scheduler.enqueue(target)
     }
 }
